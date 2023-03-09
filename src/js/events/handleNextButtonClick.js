ac1c37d70ea9206bdcea4a6dec8c5e2abb8c654e
jsh3418 <albert5428@gmail.com>
2023-03-10 01:01:27 +0900

    ♻️ refactor: 불필요한 함수 호출하지 않도록 수정

--- src/js/events/handleNextButtonClick.js
+++ src/js/events/handleNextButtonClick.js
@@ -1,13 +1,14 @@
 import MONTH_NAMES from "../constants/MONTH_NAMES.js";
 
 export default () => {
-  const $year = document.querySelector("#year");
   const $month = document.querySelector("#month");
   const $monthName = document.querySelector("#month-name");
 
   $month.textContent = Number($month.textContent) + 1;
 
   if ($month.textContent > 12) {
+    const $year = document.querySelector("#year");
+
     $year.textContent = Number($year.textContent) + 1;
     $month.textContent = 1;
   }

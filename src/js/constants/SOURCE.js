4484836965eb6506c20db0ac6aef7f86ce9efb95
jsh3418 <albert5428@gmail.com>
2023-03-09 23:37:42 +0900

    ✨ feat: source 경로 상수화

---
+++ src/js/constants/SOURCE.js
@@ -0,0 +1,10 @@
+export default {
+  CHECK_BOX_DEFAULT: "/src/assets/check-box-default.svg",
+  CHECK_BOX_SELECTED: "/src/assets/check-box-selected.svg",
+  DROPDOWN_ICON: "/src/assets/dropdown-icon.svg",
+  LEFT_ARROW: "/src/assets/left_arrow.svg",
+  MINUS_ICON: "/src/assets/minus-icon.svg",
+  PLUS_ICON: "/src/assets/plus-icon.svg",
+  RIGHT_ARROW: "/src/assets/right_arrow.svg",
+  SUBMIT_BUTTON: "/src/assets/submit-button.svg",
+};

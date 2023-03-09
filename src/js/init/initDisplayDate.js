0d4f09042c91319ad4d8c7cac8a03bd5e3c9413d
jsh3418 <albert5428@gmail.com>
2023-03-09 22:59:46 +0900

    ♻️ refactor: getCurrentDate 함수 리턴값 중 monthName 제거

--- src/js/init/initDisplayDate.js
+++ src/js/init/initDisplayDate.js
@@ -2,7 +2,8 @@ import getCurrentDate from "../utils/getCurrentDate.js";
 import monthNames from "../constants/monthNames.js";
 
 export default () => {
-  const { year, month, monthName } = getCurrentDate();
+  const { year, month } = getCurrentDate();
+  const monthName = monthNames[month - 1];
 
   const $year = document.querySelector("#year");
   const $month = document.querySelector("#month");
--- src/js/utils/getCurrentDate.js
+++ src/js/utils/getCurrentDate.js
@@ -1,11 +1,8 @@
-import monthNames from "../constants/monthName.js";
-
 export default () => {
   const currentDate = new Date();
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth() + 1;
   const date = currentDate.getDate();
-  const monthName = monthNames[month - 1];
 
-  return { year, month, date, monthName };
+  return { year, month, date };
 };

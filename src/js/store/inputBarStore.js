1f8e2884b8b8bba6247c4677d67158fb8f5c265d
jsh3418 <albert5428@gmail.com>
2023-03-12 20:57:14 +0900

    ♻️ refactor: TRANSACTION_TYPE 상수화

--- src/js/store/inputBarStore.js
+++ src/js/store/inputBarStore.js
@@ -1,21 +1,23 @@
+import { TRANSACTION_TYPE } from "../constants/TRANSACTION_TYPE.js";
+
 export const inputBarStore = {
-  transactionType: "expenditure",
+  transactionType: TRANSACTION_TYPE.EXPENDITURE,
 
   toggleTransactionType() {
-    if (this.transactionType === "expenditure") {
-      this.transactionType = "income";
+    if (this.transactionType === TRANSACTION_TYPE.EXPENDITURE) {
+      this.transactionType = TRANSACTION_TYPE.INCOME;
 
       return;
     }
 
-    if (this.transactionType === "income") {
-      this.transactionType = "expenditure";
+    if (this.transactionType === TRANSACTION_TYPE.INCOME) {
+      this.transactionType = TRANSACTION_TYPE.EXPENDITURE;
 
       return;
     }
   },
 
   isTransactionExpenditure() {
-    return this.transactionType === "expenditure";
+    return this.transactionType === TRANSACTION_TYPE.EXPENDITURE;
   },
 };

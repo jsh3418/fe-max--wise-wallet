fe63396b831080897a17edab9890be21470c90a0
jsh3418 <albert5428@gmail.com>
2023-03-14 13:40:40 +0900

    ✨ feat: inputBar 상태에 따라 분류 드롭다운 리스트 변하도록 구현

--- src/js/events/inputBar/handleTransactionTypeClick.js
+++ src/js/events/inputBar/handleTransactionTypeClick.js
@@ -1,14 +1,32 @@
 import { SOURCE } from "../../constants/SOURCE.js";
 import { inputBarStore } from "../../store/inputBarStore.js";
+import { expenditureTemplate } from "../../views/category/expenditure.js";
+import { incomeTemplate } from "../../views/category/income.js";
 
 export const handleTransactionTypeClick = ({ target }) => {
   inputBarStore.toggleTransactionType();
+  toggleIconSource(target);
+  reRenderCategoryList();
+};
 
+const toggleIconSource = (target) => {
   if (inputBarStore.isTransactionExpenditure()) {
-    target.src = SOURCE.PLUS_ICON;
+    target.src = SOURCE.MINUS_ICON;
 
     return;
   }
 
-  target.src = SOURCE.MINUS_ICON;
+  target.src = SOURCE.PLUS_ICON;
+};
+
+const reRenderCategoryList = () => {
+  const $ul = document.querySelector(".category-dropdown__ul");
+
+  if (inputBarStore.isTransactionExpenditure()) {
+    $ul.innerHTML = expenditureTemplate;
+
+    return;
+  }
+
+  $ul.innerHTML = incomeTemplate;
 };

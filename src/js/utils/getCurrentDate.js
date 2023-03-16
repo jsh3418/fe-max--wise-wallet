2ec478fadc98773fdd6a9d26ca69179300faa797
jsh3418 <albert5428@gmail.com>
2023-03-16 16:37:21 +0900

    ✨ feat: getCurrentDate 함수 리턴값 day 추가

--- src/js/utils/getCurrentDate.js
+++ src/js/utils/getCurrentDate.js
@@ -3,6 +3,7 @@ export const getCurrentDate = () => {
   const year = currentDate.getFullYear();
   const month = currentDate.getMonth() + 1;
   const date = currentDate.getDate();
+  const day = currentDate.getDay();
 
-  return { year, month, date };
+  return { year, month, date, day };
 };

3f5ccd5d3f3024e8ab9074faa2a3ec7a091e6b57
jsh3418 <albert5428@gmail.com>
2023-03-16 16:41:26 +0900

    🐛 fix: inputBar 일자 초기화 시 day까지 표시되어 수정

--- src/js/init/initInputBarDate.js
+++ src/js/init/initInputBarDate.js
@@ -2,7 +2,8 @@ import { getCurrentDate } from "../utils/getCurrentDate.js";
 
 export const initInputBarDate = () => {
   const $date = document.querySelector("#input-bar__date");
-  const currentDate = getCurrentDate();
+  const { year, month, date } = getCurrentDate();
+  const currentDate = { year, month, date };
 
   $date.value = Object.values(currentDate).reduce((acc, cur) => (cur < 10 ? acc.toString() + "0" + cur : acc + cur));
 };

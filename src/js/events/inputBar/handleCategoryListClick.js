7a42aee4bdef6a845dbd607da461c9131520369f
jsh3418 <albert5428@gmail.com>
2023-03-17 13:19:39 +0900

    🐛 fix: 카테고리 ul 클릭 시 오작동 현상 수정

--- src/js/events/inputBar/handleCategoryListClick.js
+++ src/js/events/inputBar/handleCategoryListClick.js
@@ -2,6 +2,8 @@ import { inputBarStore } from "../../store/inputBarStore.js";
 import { hideElement } from "../../utils/hideElement.js";
 
 export const handleCategoryListClick = ({ target, currentTarget }) => {
+  if (target.tagName === "UL") return;
+
   const $categoryText = document.querySelector(".input-bar__category-text");
 
   $categoryText.textContent = target.textContent;

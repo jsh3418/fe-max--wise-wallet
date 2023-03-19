f6127c18a28a97784b3ea52916eedd80193b402b
jsh3418 <albert5428@gmail.com>
2023-03-20 01:03:08 +0900

    ♻️ refactor: 동작을 함수로 분리, 파싱 단계에서 검증하도록 리팩토링

--- src/js/events/inputBar/handleDateChange.js
+++ src/js/events/inputBar/handleDateChange.js
@@ -2,20 +2,27 @@ import { initInputBarDate } from "../../init/initInputBar.js";
 import { inputBarStore } from "../../store/inputBarStore.js";
 
 export const handleDateChange = ({ target }) => {
-  const inputDate = target.value;
-  const isValidDate = /^(1000|1\d{3}|[2-9]\d{3})((0[1-9])|(1[0-2]))((0[1-9])|([1-2][0-9])|(3[0-1]))$/.test(inputDate);
+  try {
+    const date = parseDate(target.value);
 
-  if (!isValidDate) {
-    alert("유효한 날짜가 아닙니다.");
+    inputBarStore.setDate(date);
+  } catch (error) {
+    alert(error.message);
     initInputBarDate();
-
-    return;
   }
+};
+
+const parseDate = (inputDate) => {
+  if (typeof inputDate !== "string") throw new Error("input 값이 문자열이 아닙니다.");
+  if (inputDate.length !== 8) throw new Error("날짜를 연월일 순으로 8자를 맞춰 입력해주세요. ex) 20230101");
 
   const year = Number(inputDate.slice(0, 4));
-  const date = Number(inputDate.slice(6, 8));
   const month = Number(inputDate.slice(4, 6));
+  const date = Number(inputDate.slice(6, 8));
   const day = new Date(year, month - 1, date).getDay();
 
-  inputBarStore.setDate({ year, date, month, day });
+  if (1 > month || month > 12) throw new Error("1월부터 12월까지만 입력해주세요.");
+  if (1 > date || date > 31) throw new Error("1일부터 31일까지만 입력해주세요.");
+
+  return { year, month, date, day };
 };

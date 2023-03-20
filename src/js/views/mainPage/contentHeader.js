7f01f032426dbede0927cd128bcc1c2921ad94a9
jsh3418 <albert5428@gmail.com>
2023-03-21 01:47:58 +0900

    ✨ feat: 수입 지출 필터 0원인 경우 숨겨지도록 구현

--- src/js/views/mainPage/contentHeader.js
+++ src/js/views/mainPage/contentHeader.js
@@ -20,30 +20,46 @@ export const getContentHeader = ({ count, incomeValue, expenditureValue }) => {
             children: [
               {
                 element: {
-                  tagName: "img",
-                  classList: ["content-header__income-filter-checkbox", "checked"],
-                  src: SOURCE.CHECK_BOX_SELECTED,
-                },
-              },
-              {
-                element: {
-                  tagName: "span",
-                  classList: ["content-header__total-income"],
-                  text: `수입 ${incomeValue.toLocaleString()}`,
-                },
-              },
-              {
-                element: {
-                  tagName: "img",
-                  classList: ["content-header__expenditure-filter-checkbox", "checked"],
-                  src: SOURCE.CHECK_BOX_SELECTED,
+                  tagName: "div",
+                  classList: incomeValue ? ["content-header__total-filter-inner"] : ["content-header__total-filter-inner", "display-none"],
+                  children: [
+                    {
+                      element: {
+                        tagName: "img",
+                        classList: ["content-header__income-filter-checkbox", "checked"],
+                        src: SOURCE.CHECK_BOX_SELECTED,
+                      },
+                    },
+                    {
+                      element: {
+                        tagName: "span",
+                        classList: ["content-header__total-income"],
+                        text: `수입 ${incomeValue.toLocaleString()}`,
+                      },
+                    },
+                  ],
                 },
               },
               {
                 element: {
                   tagName: "div",
-                  classList: ["content-header__total-expenditure"],
-                  text: `지출 ${expenditureValue.toLocaleString()}`,
+                  classList: expenditureValue ? ["content-header__total-filter-inner"] : ["content-header__total-filter-inner", "display-none"],
+                  children: [
+                    {
+                      element: {
+                        tagName: "img",
+                        classList: ["content-header__expenditure-filter-checkbox", "checked"],
+                        src: SOURCE.CHECK_BOX_SELECTED,
+                      },
+                    },
+                    {
+                      element: {
+                        tagName: "div",
+                        classList: ["content-header__total-expenditure"],
+                        text: `지출 ${expenditureValue.toLocaleString()}`,
+                      },
+                    },
+                  ],
                 },
               },
             ],

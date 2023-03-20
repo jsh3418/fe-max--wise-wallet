322faa97334f578b4cb699b2db4538a992d20954
jsh3418 <albert5428@gmail.com>
2023-03-20 16:54:01 +0900

    ♻️ refactor: 사용자의 입력값을 구문 분석, 예외 처리하도록 수정

--- src/js/events/inputBar/handlePriceInputBlur.js
+++ src/js/events/inputBar/handlePriceInputBlur.js
@@ -1,10 +1,27 @@
 import { inputBarStore } from "../../store/inputBarStore.js";
 
 export const handlePriceInputBlur = ({ target }) => {
-  inputBarStore.setPrice(target.value);
-  renderFormatNumber(target);
+  const inputPrice = target.value;
+
+  try {
+    const price = parsePrice(inputPrice);
+
+    inputBarStore.setPrice(price);
+    renderFormatPrice(price, target);
+  } catch (error) {
+    alert(error.message);
+    target.value = "";
+  }
 };
 
-const renderFormatNumber = (target) => {
-  target.value = Number(target.value).toLocaleString("ko-KR");
+const parsePrice = (inputPrice) => {
+  const price = Number(inputPrice);
+
+  if (isNaN(price)) throw new Error("금액 입력 값에 숫자 외 다른 값이 들어왔습니다.");
+
+  return price;
+};
+
+const renderFormatPrice = (price, target) => {
+  target.value = price.toLocaleString("ko-KR");
 };
